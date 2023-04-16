5e84d36bad2737a89a2ba9debc4233f3b61a415b
M.A <md.ali2001@hotmail.com>
2023-04-16 04:48:50 -0700

    designing home tab

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,55 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(color: Colors.blue);
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Row(children: <Widget>[
+            Container(
+              width: 360,
+              height: 60,
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          Icons.person_pin,
+                          size: 30,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 2),
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(60)),
+                            side: BorderSide(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "What's on your mind?       ",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]),
+          Divider(color: Colors.black)
+        ],
+      ),
+    );
   }
 }
