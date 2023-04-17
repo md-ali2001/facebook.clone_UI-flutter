33e77fe5ab0dcc1245c26c777ca32f0553f8700f
M.A <md.ali2001@hotmail.com>
2023-04-17 12:07:29 -0700

    simplifying code in previous commit

--- lib/home.dart
+++ lib/home.dart
@@ -9,55 +9,93 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          Row(children: <Widget>[
-            Container(
-              width: 360,
-              height: 60,
-              child: Row(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(
-                          Icons.person_pin,
-                          size: 30,
-                        ),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, top: 2),
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(60)),
-                            side: BorderSide(
-                              width: 1,
-                              color: Colors.black,
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            "What's on your mind?       ",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ),
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 415, left: 0),
+        child: Container(
+          child: Row(
+            children: [
+              Container(
+                  height: 60,
+                  width: 50,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.person_pin,
+                      size: 30,
+                    ),
+                    onPressed: () {},
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 6, left: 5),
+                child: Container(
+                  height: 40,
+                  width: 240,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60)),
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.black,
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "What's on your mind?       ",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ]),
-          Divider(color: Colors.black)
-        ],
-      ),
-    );
+            ],
+          ),
+
+          //   child: ListView(
+          // children: <Widget>[
+          //   Row(children: <Widget>[
+          //     Container(
+          //       width: 360,
+          //       height: 60,
+          //       child: Row(
+          //         children: <Widget>[
+          //           Column(
+          //             children: <Widget>[
+          //               IconButton(
+          //                 icon: Icon(
+          //                   Icons.person_pin,
+          //                   size: 30,
+          //                 ),
+          //                 onPressed: () {},
+          //               )
+          //             ],
+          //           ),
+          //           Column(
+          //             children: <Widget>[
+          //               Padding(
+          //                 padding: const EdgeInsets.only(left: 10, top: 2),
+          //                 child: OutlinedButton(
+          //                   style: OutlinedButton.styleFrom(
+          //                     shape: RoundedRectangleBorder(
+          //                         borderRadius: BorderRadius.circular(60)),
+          //                     side: BorderSide(
+          //                       width: 1,
+          //                       color: Colors.black,
+          //                     ),
+          //                   ),
+          //                   onPressed: () {},
+          //                   child: Text(
+          //                     "What's on your mind?       ",
+          //                     style: TextStyle(color: Colors.grey),
+          //                   ),
+          //                 ),
+          //               ),
+          //             ],
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   ]),
+          //   Divider(color: Colors.black)
+          // ],
+        ));
   }
 }
