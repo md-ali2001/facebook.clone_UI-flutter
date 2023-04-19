1a55ccc22944b51f0dbfcf8a52583131ccd35e34
M.A <md.ali2001@hotmail.com>
2023-04-19 08:19:10 -0700

    creating friendpost widget (in process)

--- lib/home.dart
+++ lib/home.dart
@@ -11,109 +11,122 @@ class _HomescreenState extends State<Homescreen> {
     // TODO: implement build
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Row(children: [
-            IconButton(
-              icon: Icon(
-                Icons.person_pin,
-                size: 30,
-              ),
-              onPressed: () {},
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 9, top: 3),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(60)),
-                  side: BorderSide(
-                    width: 1,
-                    color: Colors.black,
-                  ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(children: [
+              IconButton(
+                icon: Icon(
+                  Icons.person_pin,
+                  size: 30,
                 ),
                 onPressed: () {},
-                child: Text(
-                  "What's on your mind?       ",
-                  style: TextStyle(color: Colors.grey),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 9, top: 3),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(60)),
+                    side: BorderSide(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "What's on your mind?       ",
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ),
               ),
-            ),
-          ]),
-          Divider(color: Colors.black),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: Container(
-                      height: 45,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.videocam,
-                              size: 30,
-                              color: Colors.red,
+            ]),
+
+            Divider(color: Colors.black),
+
+            Container(
+              height: 27,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Container(
+                          height: 27,
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.center,
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 12.0),
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.videocam,
+                                    size: 25,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 4),
+                                child: Text("Live",
+                                    style: TextStyle(fontSize: 15)),
+                              )
+                            ],
+                          ))),
+                  Expanded(
+                    child: Container(
+                        height: 30,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.photo_album,
+                                size: 25,
+                                color: Colors.green,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(right: 4),
-                            child: Text("Live", style: TextStyle(fontSize: 15)),
-                          )
-                        ],
-                      ))),
-              Expanded(
-                child: Container(
-                    height: 45,
+                            Padding(
+                              padding: EdgeInsets.only(right: 4),
+                              child:
+                                  Text("Photo", style: TextStyle(fontSize: 15)),
+                            )
+                          ],
+                        )),
+                  ),
+                  Container(
+                    height: 30,
+                    width: 145,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         IconButton(
                           onPressed: () {},
                           icon: Icon(
-                            Icons.photo_album,
-                            size: 30,
-                            color: Colors.green,
+                            Icons.location_on,
+                            size: 25,
+                            color: Colors.pink,
                           ),
                         ),
                         Padding(
-                          padding: EdgeInsets.only(right: 4),
-                          child: Text("Photo", style: TextStyle(fontSize: 15)),
+                          padding: EdgeInsets.only(right: 15),
+                          child: Text(
+                            "Check in",
+                            style: TextStyle(fontSize: 15),
+                          ),
                         )
                       ],
-                    )),
-              ),
-              Container(
-                height: 45,
-                width: 145,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.location_on,
-                        size: 30,
-                        color: Colors.pink,
-                      ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 15),
-                      child: Text(
-                        "Check in",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          Container(height: 10, color: Colors.black26),
-          Expanded(
-            child: Container(
+                  )
+                ],
+              ),
+            ),
+            Container(height: 7, color: Colors.black26),
+
+            Container(
+              height: 155,
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
@@ -135,58 +148,59 @@ class _HomescreenState extends State<Homescreen> {
                 ),
               ),
             ),
-          ),
-          Container(height: 10, color: Colors.black26),
-          Expanded(child: Container())
-        ],
+            Container(height: 7, color: Colors.black26),
+            friendpost(),
+            //friendpost()
+          ],
 
-        //   child: ListView(
-        // children: <Widget>[
-        //   Row(children: <Widget>[
-        //     Container(
-        //       width: 360,
-        //       height: 60,
-        //       child: Row(
-        //         children: <Widget>[
-        //           Column(
-        //             children: <Widget>[
-        //               IconButton(
-        //                 icon: Icon(
-        //                   Icons.person_pin,
-        //                   size: 30,
-        //                 ),
-        //                 onPressed: () {},
-        //               )
-        //             ],
-        //           ),
-        //           Column(
-        //             children: <Widget>[
-        //               Padding(
-        //                 padding: const EdgeInsets.only(left: 10, top: 2),
-        //                 child: OutlinedButton(
-        //                   style: OutlinedButton.styleFrom(
-        //                     shape: RoundedRectangleBorder(
-        //                         borderRadius: BorderRadius.circular(60)),
-        //                     side: BorderSide(
-        //                       width: 1,
-        //                       color: Colors.black,
-        //                     ),
-        //                   ),
-        //                   onPressed: () {},
-        //                   child: Text(
-        //                     "What's on your mind?       ",
-        //                     style: TextStyle(color: Colors.grey),
-        //                   ),
-        //                 ),
-        //               ),
-        //             ],
-        //           )
-        //         ],
-        //       ),
-        //     ),
-        //   ]),
-        //   Divider(color: Colors.black)
-        // ],
+          //   child: ListView(
+          // children: <Widget>[
+          //   Row(children: <Widget>[
+          //     Container(
+          //       width: 360,
+          //       height: 60,
+          //       child: Row(
+          //         children: <Widget>[
+          //           Column(
+          //             children: <Widget>[
+          //               IconButton(
+          //                 icon: Icon(
+          //                   Icons.person_pin,
+          //                   size: 30,
+          //                 ),
+          //                 onPressed: () {},
+          //               )
+          //             ],
+          //           ),
+          //           Column(
+          //             children: <Widget>[
+          //               Padding(
+          //                 padding: const EdgeInsets.only(left: 10, top: 2),
+          //                 child: OutlinedButton(
+          //                   style: OutlinedButton.styleFrom(
+          //                     shape: RoundedRectangleBorder(
+          //                         borderRadius: BorderRadius.circular(60)),
+          //                     side: BorderSide(
+          //                       width: 1,
+          //                       color: Colors.black,
+          //                     ),
+          //                   ),
+          //                   onPressed: () {},
+          //                   child: Text(
+          //                     "What's on your mind?       ",
+          //                     style: TextStyle(color: Colors.grey),
+          //                   ),
+          //                 ),
+          //               ),
+          //             ],
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   ]),
+          //   Divider(color: Colors.black)
+          // ],
+        ),
       ),
     );
   }
@@ -269,4 +283,24 @@ Widget friendstory({picture, name, profpic}) {
           )
         ],
       ));
+}
+
+Widget friendpost() {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Container(height: 50, width: 60, color: Colors.black),
+          Column(
+            children: [
+              Container(height: 15, width: 240, color: Colors.green),
+              Container(height: 35, width: 240, color: Colors.yellow)
+            ],
+          ),
+          Container(height: 50, width: 60, color: Colors.black)
+        ],
+      ),
+      Container(height: 450, width: 360, color: Colors.red)
+    ],
+  );
 }
